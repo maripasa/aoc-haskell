13ea14028b0ed8ff26fd4f8dc65ecf132ef9dc23
maripasa <asafexi@proton.me>
2025-12-02 05:05:10 -0300

    Made the name make a lil more sense

--- 2025/01/Main.hs
+++ 2025/01/Main.hs
@@ -1,10 +1,9 @@
-
-flipDialCount r s
+turn r s
   | r > 0             = (c , s')
   | s == 0 && s' > 0  = (c - 1, s')
   | s > 0  && s' == 0 = (c + 1, s')
   | otherwise         = (c, s')
   where (c, s') = (\(a,b) -> (abs a, b)) $ ((s + r) `divMod` 100)
 
-main = readFile "input" >>= \file -> return . foldl (\(c, h, s) r -> (\(c', s') -> (c + c', h + (fromEnum (s' == 0)), s')) $ flipDialCount r s) (0,0,50) . map (\(r:num) -> if r == 'R' then read num else -(read num)) . words $ file
+main = readFile "input" >>= \file -> return . foldl (\(c, h, s) r -> (\(c', s') -> (c + c', h + (fromEnum (s' == 0)), s')) $ turn r s) (0,0,50) . map (\(r:num) -> if r == 'R' then read num else -(read num)) . words $ file
 
