342315901944087520564526d3b7fdba3ddc99f0
maripasa <asafexi@proton.me>
2025-12-02 04:53:38 -0300

    tinyfied 01 and added .gitignore to comply with erics rules

--- 2025/01/Main.hs
+++ 2025/01/Main.hs
@@ -1,52 +1,10 @@
-import Debug.Trace (traceShowId)
 
-parse :: String -> [Either Int Int]
-parse input = map parseOne (words input)
-  where
-    parseOne ('L':num) = Left (read num)
-    parseOne ('R':num) = Right (read num)
-    parseOne _ = error "what"
+flipDialCount r s
+  | r > 0             = (c , s')
+  | s == 0 && s' > 0  = (c - 1, s')
+  | s > 0  && s' == 0 = (c + 1, s')
+  | otherwise         = (c, s')
+  where (c, s') = (\(a,b) -> (abs a, b)) $ ((s + r) `divMod` 100)
 
-part1 :: [Either Int Int] -> Int
-part1 rotations = rotate rotations 50 0
-  where
-    rotate (r:rs) state zeros =
-      let s' = flipDial r state
-      in rotate rs s' (zeros + fromEnum (s' == 0))
-    rotate [] _ zeros = zeros
+main = readFile "input" >>= \file -> return . foldl (\(c, h, s) r -> (\(c', s') -> (c + c', h + (fromEnum (s' == 0)), s')) $ flipDialCount r s) (0,0,50) . map (\(r:num) -> if r == 'R' then read num else -(read num)) . words $ file
 
-flipDial :: Either Int Int -> Int -> Int
-flipDial (Right amount) start = (start + amount) `mod` 100
-flipDial (Left amount) start = (start - amount) `mod` 100
-
-flipDialCount :: Either Int Int -> Int -> (Int, Int)
-flipDialCount (Right amount) start =
-    let end   = (start + amount) `mod` 100
-        wraps = (start + amount) `div` 100
-    in (end, wraps)
-
-flipDialCount (Left amount) start =
-    let (a, b) = (start - amount) `divMod` 100
-        base   = abs a
-        extra
-          | start == 0 && b > 0 = -1
-          | start > 0  && b == 0 = 1
-          | otherwise            = 0
-    in (b, base + extra)
-
-part2 :: [Either Int Int] -> Int
-part2 rotations = rotate rotations 50 0
-  where
-    rotate (r:rs) state zeros =
-      let (s', skips) = flipDialCount r state
-      in rotate rs s' (zeros + skips)
-    rotate [] _ zeros = zeros
-
-
-main :: IO ()
-main =
-  readFile "input" >>= \file -> do
-    let content = parse file
-    print content
-    (print . part1) content
-    (print . part2) content
