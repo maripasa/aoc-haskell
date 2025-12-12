7606b71829c0760a05c22403fb3b8495913ca8f4
maripasa <asafexi@proton.me>
2025-12-12 10:06:01 -0300

    Will test on stronger computers

--- 2025/09/Main.hs
+++ 2025/09/Main.hs
@@ -1,97 +1,68 @@
-import Data.List
+import Data.List (tails)
 
--- LONG LIST OF MAYBE USEFUL FUNCTIONS.
--- 
--- isCollinearPointsGrid (x1, y1) (x2, y2) (x3, y3) = (x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)
--- corner a b c = not $ isCollinearPointsGrid a b c
--- corners xs = corners' ([last xs] ++ xs ++ [head xs])
---   where
---     corners' (a:b:c:rest)
---       | corner a b c = b : corners' (b:c:rest)
---       | otherwise    = corners' (b:c:rest)
---     corners' _ = []
--- inside xs ys = all (\(a, b) -> (isCollinearLinesGrid a b) || not (intersectsLinesGrid a b)) $ [(a,b) | a <- sides xs , b <- sides ys]
--- intersectsLinesGrid ((a, b), (c, d)) ((e, f), (g, h)) = (between e g a || between e g c) && (between b d f || between b d h)
--- isCollinearLinesGrid ((a,b),(c,d)) ((e,f), (g,h)) = (a == e && c == g) || (b == f && d == h)
---
+type Pt  = (Int, Int)
+type Seg = (Pt, Pt)
 
--- ..............
--- .......#XWXW..
--- .......X...W..
--- ..#XXXX#...W..
--- ..X.....3OOOOO
--- ..#XXXXXX#.W..
--- .........X.W..
--- .........#XW..
--- ..............
---
--- point is not on top of side && line to right crosses
+readCoords :: String -> [Pt]
+readCoords s = map (\x -> read ("(" ++ x ++ ")")) (lines s)
 
-inside edges (a,b) = all (\lin -> (any (crosses lin) edges)) linesFromPoint
-  where
-    linesFromPoint =
-      [ ((a, b), (a, b + 20000))
-      , ((a, b), (a, b - 20000))
-      , ((a, b), (a + 20000, b))
-      , ((a, b), (a - 20000, b))
-      ]
+edges :: [Pt] -> [Seg]
+edges xs = zip xs (tail (cycle xs))
 
-between a b x = x >= min a b && x <= max a b
+comb :: [a] -> [(a,a)]
+comb xs = [(a,b) | (a,rest) <- zip xs (tail (tails xs)), b <- rest]
 
-crosses a b
-  | vertical a && horizontal b = crossesHV b a
-  | horizontal a && vertical b = crossesHV a b
+areaByDiag :: Pt -> Pt -> Int
+areaByDiag (a,b) (c,d) =
+  (abs (a - c) + 1) * (abs (b - d) + 1)
+
+pointsRect :: Pt -> Pt -> [Pt]
+pointsRect (a,b) (c,d) =
+  [ (x,y)
+  | x <- [min a c .. max a c]
+  , y <- [min b d .. max b d]
+  ]
+
+pointOnSegment :: Pt -> Seg -> Bool
+pointOnSegment (px,py) ((x1,y1),(x2,y2))
+  | x1 == x2 =
+      px == x1 && py >= min y1 y2 && py <= max y1 y2
+  | y1 == y2 =
+      py == y1 && px >= min x1 x2 && px <= max x1 x2
   | otherwise = False
-  where
-    crossesHV ((a, b), (c, d)) ((e, f), (g, h)) = between a c e && between f h b
 
-vertical ((a,b),(c,d)) = a == c
-horizontal ((a,b), (c,d)) = b == d
+rayIntersects :: Pt -> Seg -> Bool
+rayIntersects (px,py) ((x1,y1),(x2,y2))
+  | y1 == y2 = False
+  | otherwise =
+      let x = x1
+          ymin = min y1 y2
+          ymax = max y1 y2
+      in  py > ymin && py <= ymax && x > px
+       
+pointInPolygon :: [Seg] -> Pt -> Bool
+pointInPolygon segs p = any (pointOnSegment p) segs || odd (length [() | seg <- segs, rayIntersects p seg])
 
--- i'll test the points to the right, so i want to count how many vertical lines it hits
-
---pointsRectDiag (a,b) (c,d) = [(a,b), (b,c), (c,d), (d,a)]
-
-otherPoints (a,b) (c,d) = [(a,d), (c,b)]
-
-comb xs = [(a,b) | (a, others) <- zip xs (tail $ tails xs), b <- others]
-
-edges xs = side (xs ++ [head xs])
-  where
-    side (a:b:rest) = (a,b) : side (b:rest)
-    side a = []
-
--- cartesian x, y, 0 based.
-
-areaByDiag (a,b) (c,d) = (abs (a - c) + 1) * (abs (b - d) + 1)
-
+main :: IO ()
 main = do
-  c <- readFile "input" >>= (\f -> return . map (\x -> read $ "(" ++ x ++ ")") . lines $ f)
-  let coords :: [(Int, Int)]
-      coords = c
-  let diags = comb coords
-      part1 = maximum . map (uncurry areaByDiag)
+  file <- readFile "input"
+  let coords = readCoords file
 
-  let polygon = edges coords
-      part2 = part1 . map (\[a,b] -> (a,b)) . filter (all (inside polygon)) . map (uncurry otherPoints) $ diags
+  let poly   = edges coords
+      diags  = comb coords
+
+  let part1Max
+    | null diags = 0
+    | otherwise  = maximum (map (uncurry areaByDiag) diags)
+
+  let validDiags =
+        [ (p,q)
+        | (p,q) <- diags
+        , all (pointInPolygon poly) (pointsRect p q)
+        ]
+
+  let part2
+    | null validDiags = 0
+    | otherwise       =  maximum (map (uncurry areaByDiag) validDiags)
 
   print part2
-
-
-cellChar :: [((Int,Int),(Int,Int))] -> (Int,Int) -> Char
-cellChar polygon p =
-    if inside polygon p
-    then 'X'
-    else '.'
-
-makeGrid :: [((Int,Int),(Int,Int))] -> [[Char]]
-makeGrid polygon =
-    [ [ cellChar polygon (x,y) | x <- [0..13] ]
-    | y <- [0..10]
-    ]
-
-printGrid :: [[Char]] -> IO ()
-printGrid = mapM_ putStrLn
-
-drawPolygon :: [((Int,Int),(Int,Int))] -> IO ()
-drawPolygon poly = printGrid (makeGrid poly)
